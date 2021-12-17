d272863d0d042155f3a881997e11bbeeb34de212
Levan Roinishvili <levanroinishvili@gmail.com>
2021-12-17 11:32:23 +0000

    Clear message box

--- 3-sample-primes.js
+++ 3-sample-primes.js
@@ -1,4 +1,9 @@
 const PRIMES = [        /*        Number       vs    BigInt   */
+
+
+    100000007, // 1e8 + 7,
+    1000000007, // 1e9 + 7,
+
     10851501389951,     //   50 milliseconds        <1 second
     70891006769753,     //  130 milliseconds        3 seconds
     881622473636603,    //  400 milliseconds        7 seconds
